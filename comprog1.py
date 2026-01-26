edfde7b56ad0fbd7e61f48f43e4133d758ace1af
hallazgochowelamae-design <hallazgo.chowelamae@gmail.com>
2026-01-26 17:46:39 +0800

    Add files via upload

---
+++ comprog1.py
@@ -0,0 +1,39 @@
+score=100
+print("Score is 100")
+if score >= 75:
+   print ("PASSED")
+else:
+    print ("FAILED")
+print()
+
+score=80
+print("Score is 80")
+if score >= 75:
+   print ("PASSED")
+else:
+    print ("FAILED")
+print()
+
+score=75
+print("Score is 75")
+if score >= 75:
+   print ("PASSED")
+else:
+    print ("FAILED")
+print()
+
+
+score=73
+print("Score is 73")
+if score >= 75:
+   print ("PASSED")
+else:
+    print ("FAILED")
+print()
+
+
+
+
+
+
+

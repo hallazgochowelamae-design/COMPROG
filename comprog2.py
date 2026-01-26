df6ef05703dd565b6757bb8a92ac839e34cc11bf
hallazgochowelamae-design <hallazgo.chowelamae@gmail.com>
2026-01-26 17:48:26 +0800

    Add files via upload

---
+++ comprog2.py
@@ -0,0 +1,25 @@
+fullname= "Chowela Mae Hallazgo"
+age=22
+height=1.60
+currently_working= False
+
+print("Fullname:", fullname)
+print(type(fullname))
+print()
+
+print("Age:", age)
+print(type(age))
+print()
+
+print("Height in meters:", height)
+print(type(height))
+print()
+
+print("Currently working?", currently_working)
+print(type(currently_working))
+
+
+
+
+
+
